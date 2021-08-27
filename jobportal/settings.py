5e818614feeeb481411b0a31936bfafd3f3a1410
Jashandeep singh <62203228+jashandeep1659@users.noreply.github.com>
2021-08-27 21:10:40 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*%!s7q2(n)#65xggcmvm0nkf2(@v#8j1&fg3icjkwkn(#nps36'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.38','job-portal-testing.herokuapp.com']
 
@@ -131,7 +131,7 @@ STATICFILES_DIRS= [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
